b821052ddb28c2ffcebaf8c167eb5969b9d01813
EmalineG <mgayhart0@ivc.edu>
2023-05-01 22:11:51 -0700

    sections outlined

---
+++ src/components/AdSpace.tsx
@@ -0,0 +1,15 @@
+const AdSpace = () => {
+    return(
+        <div className="ad-space">
+            <div className="ad-space__container">
+                <div className="ad-space__container__content">
+                    <h1 className="ad-space__container__content__title">Ad Space</h1>
+                    <p className="ad-space__container__content__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <button className="ad-space__container__content__button">Learn More</button>
+                </div>
+            </div>
+        </div>
+        
+    )
+}   
+export default AdSpace;
