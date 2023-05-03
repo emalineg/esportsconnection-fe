d8969088f2c53355c973ef42e66c3a06f456a849
EmalineG <mgayhart0@ivc.edu>
2023-05-03 13:11:34 -0700

    events

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -17,6 +17,12 @@ const Events = () => {
 
                 </div>
             </div>
+            <div className="grid grid-cols-3 gap-4 pt-4">
+                <div className="bg-indigo-500 rounded-md">UCI EVENTS</div>
+                <div className="bg-indigo-500 rounded-md">ALL EVENTS</div>
+                <div className="bg-indigo-500 rounded-md">IVC EVENTS</div>
+                
+            </div>
            
         </div>
         
