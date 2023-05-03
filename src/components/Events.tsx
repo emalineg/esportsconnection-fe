4232e22a29b1075d19da5efe5c72fda343de178d
EmalineG <mgayhart0@ivc.edu>
2023-05-03 13:07:39 -0700

    events

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -5,11 +5,11 @@ const Events = () => {
                 <h1>LOCAL EVENTS:</h1>
                 <div className="bg-indigo-500">
                     <div className="grid grid-cols-2 gap-4">
-                        <div className="grid grid-rows-2 gap-4">
+                        <div className="grid grid-rows-2 m-5">
                            <div>[event image]</div>
                            <div>[title of event]</div>
                         </div>
-                        <div className="grid grid-rows-2 gap-4">
+                        <div className="grid grid-rows-2 m-5">
                            <div>[event image]</div>
                            <div>[title of event]</div>
                         </div>
--- src/components/Podcast.tsx
+++ src/components/Podcast.tsx
@@ -5,21 +5,27 @@ const Podcast = () => {
                 <div className="text-white" >
                     <div>
                         <h1>LISTEN TO OUR LATEST INTERVIEW</h1>
-                        <div className="pl-5">
+                        <div className="pl-5 flex items-center justify-center pb-4">
                             <div>
-                                podcast embed
+                                [podcast embed]
                             </div>
                         </div>
                      </div>
                     <div>
                         <h2>OTHER RECENT INTERVIEWS</h2>
-                        <div className="grid grid-cols-4 gap-4">
+                        <div>
                             <div>
-                            <div className="pl-5">
-                        <div className="col-start-1 col-end-4 bg-indigo-500 rounded-md">list</div>
+                            <div className="grid grid-rows-2 gap-4 pl-5">
+                     
+                        <div className=" bg-indigo-500 rounded-md">[Guest Name, Insitution] [Podcast Icons]</div>
+                        <div className=" bg-indigo-500 rounded-md">[Guest Name, Insitution] [Podcast Icons]</div>
+      
                              </div>
+                             <div>
+                    <p className="text-xs text-right underline">More Podcasts!</p>
+                </div>
                              </div>
-                        <div className="col-start-4 col-end-4">icons</div>
+                
                         </div>
                     </div>
                 </div>
