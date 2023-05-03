60392df0b318481bdb7dae7c40a194f62a1fe596
EmalineG <mgayhart0@ivc.edu>
2023-05-03 13:36:05 -0700

    footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
+              <a href="https://octalkradio.com/" className="flex items-center">
                   <img src="logo.png" className="h-20 mr-3" alt="Esports Connection OC Talk Radio Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">OC Talk Radio</span>
               </a>
