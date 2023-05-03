5342d76d6f357e5a53b423e47b580c51a909d3db
EmalineG <mgayhart0@ivc.edu>
2023-05-03 13:09:43 -0700

    ad bg

--- src/components/AdSpace.tsx
+++ src/components/AdSpace.tsx
@@ -1,10 +1,10 @@
 const AdSpace = () => {
     return(
-        <div className="grid grid-cols-4 gap-4">
-            <div className="bg-indigo-500 rounded-md">ad</div>
-            <div className="bg-indigo-500 rounded-md">ad</div>
-            <div className="bg-indigo-500 rounded-md">ad</div>
-            <div className="bg-indigo-500 rounded-md">ad</div>
+        <div className="grid grid-cols-4 gap-4 bg-white">
+            <div className="bg-indigo-500 rounded-md p-2 m-5">ad</div>
+            <div className="bg-indigo-500 rounded-md p-2 m-5">ad</div>
+            <div className="bg-indigo-500 rounded-md p-2 m-5">ad</div>
+            <div className="bg-indigo-500 rounded-md p-2 m-5">ad</div>
         </div>
         
     )
