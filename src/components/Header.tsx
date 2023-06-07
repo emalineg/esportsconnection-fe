056840d58105164576a23764714db73153cf3d3c
Liz Ainslie <lizainslie16@gmail.com>
2023-06-06 19:32:30 -0700

    feat(styles) : iconography & header interactions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,41 @@
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube, faTwitch, faInstagram, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Header = () => {
     return(
-        <div className="flex flex-col items-center justify-center text-indigo-900">
+        <header className="flex flex-col items-center gap-3 justify-center text-indigo-900">
             <div className='flex flex-col items-center'>
                 <Image src="/logo.png" alt="OC Talk Radios Esports Connection Logo" width={400} height={400}/>
                 <h1 className="text-2xl text-center font-bold">WHERE THE ESPORTS WORLD GOES TO CONNECT.</h1>
             </div>
-            <div className="grid grid-cols-3 gap-4 pt-5">
-                <div className="col-start-1 col-end-1">
-                    socials
+            <div className="flex flex-row gap-4 justify-between items-center w-full px-4">
+                <div className="flex flex-row gap-2 text-indigo-800">
+                    <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                        <FontAwesomeIcon icon={faYoutube} fixedWidth />
+                    </a>
+
+                    <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                        <FontAwesomeIcon icon={faTwitch} fixedWidth />
+                    </a>
+                    
+                    <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                        <FontAwesomeIcon icon={faInstagram} fixedWidth />
+                    </a>
+
+                    <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                        <FontAwesomeIcon icon={faLinkedinIn} fixedWidth />
+                    </a>
+
+                    <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                        <FontAwesomeIcon icon={faTwitter} fixedWidth />
+                    </a>
                 </div>
-                <div className="col-start-3 col-end-3">
-                    newsletter
-                </div>
+                <button className="flex flex-row gap-2 bg-indigo-600 font-semibold p-2 text-white rounded-md cursor-pointer">
+                    Our Newsletter
+                </button>
             </div>
-        </div>
+        </header>
     );
 };
 
--- src/components/Podcast.tsx
+++ src/components/Podcast.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+
 const Podcast = () => {
     return (
         <div className="text-indigo-900 w-full flex flex-col gap-4">
@@ -11,21 +14,25 @@ const Podcast = () => {
             </div>
             <div className="flex flex-col gap-4">
                 <h2 className="text-lg font-bold ml-4">OTHER RECENT INTERVIEWS:</h2>
-                <div className="flex flex-col gap-4 px-8">
-                    <div className="flex flex-row gap-6">
+                <div className="flex flex-col gap-4 px-4 lg:px-8">
+                    <div className="flex flex-row items-center gap-3">
                         <div className="rounded-md p-2 flex-grow bg-indigo-100">
                             MARK DEPPE, UCI ESPORTS DIRECTOR
                         </div>
-                        <div className="flex flex-row gap-4">
-                            {/* Icons */}
+                        <div className="flex flex-row gap-2">
+                            <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                                <FontAwesomeIcon icon={faSpotify} fixedWidth />
+                            </a>
                         </div>
                     </div>
-                    <div className="flex flex-row gap-6">
+                    <div className="flex flex-row items-center gap-3">
                         <div className="rounded-md p-2 flex-grow bg-indigo-100">
                             ADAM LOPEZ, IVC COORDINATOR
                         </div>
-                        <div className="flex flex-row gap-4">
-                            {/* Icons */}
+                        <div className="flex flex-row gap-2">
+                            <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                                <FontAwesomeIcon icon={faSpotify} fixedWidth />
+                            </a>
                         </div>
                     </div>
                 </div>
