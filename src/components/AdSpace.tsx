f7d7126d6eb925171ae49194925a05e703019569
Liz Ainslie <lizainslie16@gmail.com>
2023-06-28 02:56:36 -0700

    feat(fe): cleanup styles, closes #2

--- src/components/AdSpace.tsx
+++ src/components/AdSpace.tsx
@@ -1,6 +1,6 @@
 const AdSpace = () => {
     return(
-        <div className="text-indigo-900 md:px-4 w-full">
+        <div className="text-indigo-900 md:px-4 w-full mb-8 mt-4">
             <div className="relative flex flex-row justify-center items-center bg-indigo-100 md:rounded-md p-4">
                 <div className="absolute top-0 right-0 bg-red-500 text-xs text-white md:rounded-tr-md rounded-bl-md p-1">Sponsored!</div>
                 [Ads]
