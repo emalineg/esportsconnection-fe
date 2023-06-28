2497e06641f8b6bbe583a23f256679e2bb0d0b4d
Liz Ainslie <lizainslie16@gmail.com>
2023-06-28 03:32:11 -0700

    feat(fe): nav & advertise page. closes #14

--- src/components/AdSpace.tsx
+++ src/components/AdSpace.tsx
@@ -1,9 +1,17 @@
+import Link from "next/link";
+
 const AdSpace = () => {
     return(
-        <div className="text-indigo-900 md:px-4 w-full mb-8 mt-4">
-            <div className="relative flex flex-row justify-center items-center bg-indigo-100 md:rounded-md p-4">
-                <div className="absolute top-0 right-0 bg-red-500 text-xs text-white md:rounded-tr-md rounded-bl-md p-1">Sponsored!</div>
-                [Ads]
+        <div className="text-indigo-800 w-full flex flex-col gap-4">
+            <div className="md:px-4 w-full mt-4">
+                <div className="relative flex flex-row justify-center items-center bg-indigo-100 md:rounded-md p-4">
+                    <div className="absolute top-0 right-0 bg-red-500 text-xs text-white md:rounded-tr-md rounded-bl-md p-1">Sponsored!</div>
+                    [Ads]
+                </div>
+            </div>
+            
+            <div className="flex flex-row justify-end px-4">
+                <Link href='/advertise' className="text-sm font-semibold text-indigo-600 text-opacity-70 hover:text-opacity-100">Advertise With Us</Link> 
             </div>
         </div>
     );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,17 @@
-const Nav = () => {
-    return(
-  <div>nav</div>
-    );
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { type FC } from "react";
+
+const Nav: FC = () => {
+  return(
+    <nav className="p-2 flex flex-row gap-4 bg-indigo-300 w-full">
+      <Link href='/' className="bg-white border border-white bg-opacity-20 border-opacity-60 hover:bg-opacity-50 text-white font-semibold px-2 py-1 flex flex-row items-center gap-1.5 rounded-md">
+        <FontAwesomeIcon icon={faHome} fixedWidth className="w-4 h-4"></FontAwesomeIcon>
+        Home
+      </Link>
+    </nav>
+  );
 }
 
 export default Nav;
