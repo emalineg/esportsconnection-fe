5857b705504a667d1979164991f47abbb8442193
Liz Ainslie <lizainslie16@gmail.com>
2023-06-28 23:47:24 -0700

    feat(fe): nav fits footer better

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,8 +5,8 @@ import { type FC } from "react";
 
 const Nav: FC = () => {
   return(
-    <nav className="p-2 flex flex-row gap-4 bg-indigo-300 w-full">
-      <Link href='/' className="bg-white border border-white bg-opacity-20 border-opacity-60 hover:bg-opacity-50 text-white font-semibold px-2 py-1 flex flex-row items-center gap-1.5 rounded-md">
+    <nav className="p-2 flex flex-row gap-4 bg-indigo-800 w-full">
+      <Link href='/' className="text-white text-opacity-70 hover:text-opacity-100 font-semibold px-2 py-1 flex flex-row items-center gap-1.5 rounded-md">
         <FontAwesomeIcon icon={faHome} fixedWidth className="w-4 h-4"></FontAwesomeIcon>
         Home
       </Link>
