ce8bb5d96ef4ca27a00fa62fbd17a94519beae54
Liz Ainslie <lizainslie16@gmail.com>
2023-07-20 20:19:32 -0700

    fix(fe): responsive icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
                 <img src="/Logo.png" alt="OC Talk Radios Esports Connection Logo" className='h-auto max-w-full md:max-w-[400px]' />
                 <h1 className="text-2xl text-center font-bold">WHERE THE ESPORTS WORLD GOES TO CONNECT.</h1>
             </div>
-            <div className="flex flex-row gap-4 justify-between items-center w-3/4 px-4">
+            <div className="flex flex-row gap-4 justify-between items-center md:w-3/4 px-4">
                 <div className="flex flex-row gap-2 text-indigo-800">
                     <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
                         <FontAwesomeIcon icon={faYoutube} fixedWidth size='2x' />
