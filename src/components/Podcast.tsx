d7730c8ee066223b4be720faca47cf00b9054e03
Liz Ainslie <lizainslie16@gmail.com>
2023-10-06 03:58:58 -0700

    feat(fe): basic podbean integration impl subject to change

--- src/components/Podcast.tsx
+++ src/components/Podcast.tsx
@@ -1,31 +1,36 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { api } from "~/utils/api";
 
 const Podcast = () => {
-    return (
+    const recentEpisodesQuery = api.misc.recentEpisodes.useQuery();
+
+    return recentEpisodesQuery.data ? (
         <div className="text-indigo-800 w-full flex flex-col gap-4">
-            <div className="flex flex-col gap-4">
+            {/* <div className="flex flex-col gap-4">
                 <h1 className="text-xl font-semibold mx-4 uppercase">Listen to Our Latest Interview</h1>
                 <div className="px-4 flex items-center justify-center">
                     <div className="rounded-lg bg-indigo-100 p-1 w-full">
                         <iframe style={{borderRadius: '8px'}} src="https://open.spotify.com/embed/episode/63xhARhXMcyOyVbE0qa00s?utm_source=generator&t=0" width="100%" height="152" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                     </div>
                 </div>
-            </div>
+            </div> */}
             <div className="flex flex-col gap-4">
-                <h2 className="text-xl font-semibold ml-4 uppercase">Other Recent Interviews</h2>
+                <h2 className="text-xl font-semibold ml-4 uppercase">{/* Other */}Recent Interviews</h2>
                 <div className="flex flex-col gap-4 px-4">
-                    <div className="flex flex-row items-center gap-3">
-                        <div className="rounded-md p-2 flex-grow bg-indigo-100 font-semibold">
-                            Mark Deppe, UCI ESports Director
+                    {recentEpisodesQuery.data.map(episode => (
+                        <div key={episode.id} className="flex flex-row items-center gap-3">
+                            <div className="rounded-md p-2 flex-grow bg-indigo-100 font-semibold">
+                                {episode.title}
+                            </div>
+                            <div className="flex flex-row gap-2">
+                                <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
+                                    <FontAwesomeIcon icon={faSpotify} fixedWidth size='2x' />
+                                </a>
+                            </div>
                         </div>
-                        <div className="flex flex-row gap-2">
-                            <a href='#' className='h-8 w-8 flex flex-row items-center justify-center'>
-                                <FontAwesomeIcon icon={faSpotify} fixedWidth size='2x' />
-                            </a>
-                        </div>
-                    </div>
-                    <div className="flex flex-row items-center gap-3">
+                    ))}
+                    {/* <div className="flex flex-row items-center gap-3">
                         <div className="rounded-md p-2 flex-grow bg-indigo-100 font-semibold">
                             Adam Lopez, IVC Coordinator
                         </div>
@@ -34,12 +39,16 @@ const Podcast = () => {
                                 <FontAwesomeIcon icon={faSpotify} fixedWidth size='2x' />
                             </a>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="flex flex-row justify-end px-4">
                     <a href='#' className="text-sm font-semibold text-indigo-600 text-opacity-70 hover:text-opacity-100">More Podcasts!</a> 
                 </div>
             </div>
+        </div>
+    ) : (
+        <div className="text-indigo-800 w-full flex flex-col gap-4">
+            <h1 className="text-xl font-semibold mx-4 uppercase">Loading episodes...</h1>
         </div>
     );
 };
--- src/server/api/routers/misc.ts
+++ src/server/api/routers/misc.ts
@@ -82,6 +82,14 @@ export const miscRouter = createTRPCRouter({
   })),
   recentEpisodes: publicProcedure.query(async ({ ctx }) => {
     const [episodes] = await ctx.podbean.fetchEpisodes(0, 3);
-    return episodes;
+
+    // tRPC doesn't like classes, only raw data. so here, we select the fields
+    // we need and nothing else. Later, we may switch this to a DB system, but
+    // we will definitely need to add more logic here to fetch and sanitize
+    // embed HTML.
+    return episodes.filter(it => it.status === 'publish').map(it => ({
+      title: it.title,
+      id: it.id,
+    }));
   }),
 });
