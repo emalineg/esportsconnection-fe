af0c1eecafe961a6584585bda3c7893d7c7672af
EmalineG <mgayhart0@ivc.edu>
2023-10-09 17:12:10 -0700

    update contact

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,15 +1,27 @@
-const Contact = () => {
+import { type NextPage } from "next";
+import Head from "next/head";
+import Footer from "~/components/Footer";
+import Nav from "~/components/Nav";
+import { NextSeo } from "next-seo";
+
+const Contact: NextPage = () => {
     return(
-        <div className="contact">
-            <div className="contact__container">
-                <div className="contact__container__content">
-                    <h1 className="contact__container__content__title">ContactPage</h1>
-                    <p className="contact__container__content__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                    <button className="contact__container__content__button">Learn More</button>
+        <>
+            <Head>
+                <NextSeo
+                    title="Contact Us"
+                />
+            </Head>
+            <main className="flex min-h-screen flex-col items-center bg-indigo-200 w-full">
+                <Nav />
+
+                <div className="container flex-grow text-indigo-900 flex flex-col items-center justify-center">
+                    <h1 className="text-5xl font-light">Contact Us </h1>
+                    <p className="text-lg leading-loose">Email us at <a href="mailto:test@email.com" target="_blank" className="underline">test@email.com</a>.</p>
                 </div>
-            </div>
-        </div>
-        
+                <Footer/>
+            </main>
+        </>
     )
 }   
 export default Contact;
