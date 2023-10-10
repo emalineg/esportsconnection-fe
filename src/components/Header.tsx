167005fe13a5a9ddda16c7f3b90c4b086b1fff93
EmalineG <mgayhart0@ivc.edu>
2023-10-09 17:48:16 -0700

    external links open in new tabs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
             </div>
             <div className="flex flex-row gap-4 justify-between items-center md:w-3/4 px-4">
                 <div className="flex flex-row gap-2 text-indigo-800">
-                    <a href='https://youtube.com/playlist?list=PLQN6TAdsJwp6jIWEOCZCCzx84ik8SdQo1&si=OOCz9eoUXgnLKuQo' className='h-8 w-8 flex flex-row items-center justify-center'>
+                    <a href='https://youtube.com/playlist?list=PLQN6TAdsJwp6jIWEOCZCCzx84ik8SdQo1&si=OOCz9eoUXgnLKuQo' target="_blank" rel="noopener noreferrer" className='h-8 w-8 flex flex-row items-center justify-center'>
                         <FontAwesomeIcon icon={faYoutube} fixedWidth size='2x' />
                     </a>
 
@@ -18,11 +18,11 @@ const Header = () => {
                         <FontAwesomeIcon icon={faTwitch} fixedWidth size='2x' />
                     </a> */}
                     
-                    <a href='https://www.instagram.com/esportsconnection/' className='h-8 w-8 flex flex-row items-center justify-center'>
+                    <a href='https://www.instagram.com/esportsconnection/' target="_blank" rel="noopener noreferrer" className='h-8 w-8 flex flex-row items-center justify-center'>
                         <FontAwesomeIcon icon={faInstagram} fixedWidth size='2x' />
                     </a> 
 
-                    <a href='https://www.linkedin.com/company/oc-talk-radio/' className='h-8 w-8 flex flex-row items-center justify-center'>
+                    <a href='https://www.linkedin.com/company/oc-talk-radio/' target="_blank" rel="noopener noreferrer" className='h-8 w-8 flex flex-row items-center justify-center'>
                         <FontAwesomeIcon icon={faLinkedinIn} fixedWidth size='2x' />
                     </a>
 
